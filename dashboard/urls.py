eb18ca6c7629bd45318fad5ce7f174b9a98145b0
Jonathan Murmu <jonathanmurmu@gmail.com>
2018-08-04 18:34:11 +0530

    modified the url and added allowed hosts

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('signin/', SignInView.as_view(), name='signin'),
-    path('logout/', logout, {"next_page": "/home/"}, name='logout'),
+    path('logout/', logout, {"next_page": "/habit-app/home/"}, name='logout'),
     path('dashboard/', DashboardView.as_view(), name='dashboard')
 ]
