1c5ef2dcade76ef721e48fc825d6db15f8edfb09
jonathanmurmu <jonathan.murmu@mindfiresolutions.com>
2018-08-10 17:39:43 +0530

    added redis url configuration

--- habit_builder/celery.py
+++ habit_builder/celery.py
@@ -15,6 +15,8 @@ app = Celery('habit_builder')
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.broker_url = 'redis://localhost:6379/0'
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
